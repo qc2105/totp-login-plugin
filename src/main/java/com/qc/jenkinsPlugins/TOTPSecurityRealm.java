3c75d67c4079797ed148c1f820cdd9c35fe07c73
qc2105 <qc2105@qq.com>
2020-06-02 09:39:29 +0800

    Add the required copy right information

--- src/main/java/com/qc/jenkinsPlugins/TOTPSecurityRealm.java
+++ src/main/java/com/qc/jenkinsPlugins/TOTPSecurityRealm.java
@@ -21,6 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 package com.qc.jenkinsPlugins;
 
 import com.thoughtworks.xstream.converters.UnmarshallingContext;
